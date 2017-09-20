349849fbc4379ce6e5ee8e32d65374775aba7282
Ben Pig Chu <benpichu@gmail.com>
2017-09-20 20:30:22 +0800

    basic file serving feature

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const fs=require("fs")
 const url=require("url")
 const path=require("path")
+const util=require("util")
 const http=require("http")
 const http2=require("http2")
 const child=require("child_process")
@@ -38,6 +39,61 @@ const{host="::",port:httpPort="80",basePath="/home/user/static/",https={},action
 const{open:httpsOpen=false,hsts=true,port:httpsPort="443",key="",cert="",renew={}}=https
 const{open:renewOpen=false,period:renewPeriod=1728000000,command:renewCommand=""}=renew
 
+const serveStream=async(context,stream)=>{
+	// TODO: encoding
+	stream.pipe(context.respond)
+}
+
+const serveConfirmedFile=async(context,filepath)=>{
+	// TODO: cache control
+	context.respond.setHeader("Content-Type",mimetype[path.extname(filepath)])
+	context.respond.writeHead(200)
+	await serveStream(context,fs.createReadStream(filepath),mimetype[path.extname(filepath)])
+}
+
+const serveFile=async(context,filepath)=>{
+	console.log(`---- [${context.reqId}] ask for ${filepath}`)
+	try{
+		await util.promisify(fs.access)(filepath,fs.constants.R_OK)
+	}catch(err){
+		console.log(`---- [${context.reqId}] not find`)
+		context.respond.writeHead(404)
+		context.respond.end("not find")
+		return
+	}
+	let stat=await util.promisify(fs.stat)(filepath)
+	if(stat.isDirectory()){
+		console.log(`---- [${context.reqId}] requested file is a directory`)
+		if(context.url.pathname[context.url.pathname.length-1]!=="/"){
+			console.log(`---- [${context.reqId}] requested path do not end with "/", 301 redirect`)
+			context.respond.writeHead(301,{Location:context.url.pathname+"/"})
+			context.respond.end()
+		}else{
+			console.log(`---- [${context.reqId}] try /index.html`)
+			let indexpath=path.join(filepath,"/index.html")
+			try{
+				await util.promisify(fs.access)(indexpath,fs.constants.R_OK)
+				if((await util.promisify(fs.stat)(indexpath)).isDirectory()){
+					throw new Error("index is directory")
+				}
+			}catch(err){
+				console.log(`---- [${context.reqId}] index not find`)
+				context.respond.writeHead(404)
+				context.respond.end("not find")
+				return
+			}
+			await serveConfirmedFile(context,indexpath)
+		}
+	}else{
+		await serveConfirmedFile(context,filepath)
+	}
+}
+
+const defaultAction=async(context)=>{
+	await serveFile(context,path.join(basePath,context.processedPath))
+	return true
+}
+
 const actionBuilders={}
 
 const actionBuilder=(schema)=>{
@@ -50,27 +106,22 @@ const actionBuilder=(schema)=>{
 
 const actions=actionsSchema.map(actionBuilder)
 
-const defaultAction=(context)=>{
-	context.respond.writeHead(200)
-	context.respond.end("hello")
-}
-
 let reqNum=0
-const handler=(req,res)=>{
+const handler=async(req,res)=>{
 	const reqId=Date.now()+reqNum
 	reqNum++
 	console.log(`-- [${reqId}] request heared at ${new Date()}`)
 	const domain=req.headers.host
 	const reqUrl=url.parse(req.url,true)
-	console.log(`---- [${reqId}] ask for "${reqUrl.pathname}" under "${domain}" with search "${reqUrl.search}>"`)
+	console.log(`---- [${reqId}] ask for "${reqUrl.pathname}" under "${domain}" with search "<${reqUrl.search}>"`)
 	const context={request:req,respond:res,domain:domain,url:reqUrl,processedPath:reqUrl.pathname,reqId:reqId}
 	for(const action of actions){
-		if(action(context)){
+		if(await action(context)){
 			return
 		}
 	}
 	console.log(`---- [${reqId}] no matched action, try serve static file`)
-	defaultAction(context)
+	await defaultAction(context)
 }
 
 let httpServer
